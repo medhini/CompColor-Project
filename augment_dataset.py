160face96bcda8eb6f72e0cfa3189f16e97b97f6
medhini <medhini95@gmail.com>
2019-11-24 03:01:44 +0000

    Fix hue shift

--- augment_dataset.py
+++ augment_dataset.py
@@ -16,34 +16,36 @@ import torchvision.transforms as transforms
 #     the num_worker of torch.utils.data.DataLoader() to 0.
 
 def extract_pallete(img):
-
     return 
 
 def hue_shift(img):
     """RGB to LAB, cache L. RGB to HSV, shift H to get H^SV. H^SV to L^A^B^. L^A^B^ to LA^B^."""
-    print(img.size)
+
     # Convert colourspace
     srgb_p = ImageCms.createProfile("sRGB")
     lab_p  = ImageCms.createProfile("LAB")
 
+    #RGB to HSV
     rgb2lab = ImageCms.buildTransformFromOpenProfiles(srgb_p, lab_p, "RGB", "LAB")
     img_lab = ImageCms.applyTransform(img, rgb2lab)
     L = np.asarray(img_lab)[:,:,0]
-
     img_hsv = img.convert('HSV')
 
+    #hue shift
     img_hsv = np.asarray(img_hsv).copy()
     h = img_hsv[:,:,0]
-    h = h+0.001
+    h = (h+50)%255
     img_hsv[:,:,0] = h
 
-    img_rgb = Image.fromarray(img_hsv).convert('RGB')
-
+    #retain original luminance
+    img_rgb = Image.fromarray(img_hsv, mode="HSV").convert('RGB')
     img_lab_t = np.asarray(ImageCms.applyTransform(img_rgb, rgb2lab)).copy()
-
     img_lab_t[:,:,0] = L
-    
-    img_recolor = Image.fromarray(img_lab_t).convert('RGB')
+
+    #convert LAB to RGB  
+    img_recolor = Image.fromarray(img_lab_t, mode="LAB")
+    lab2rgb = ImageCms.buildTransformFromOpenProfiles(lab_p, srgb_p, "LAB", "RGB")
+    img_recolor = ImageCms.applyTransform(img_recolor, lab2rgb)
     
     img_recolor.save('recolor.jpg')
 
@@ -73,8 +75,6 @@ if __name__ == '__main__':
 
         # image_palette = extract_pallete(image)
         image_recolorized = hue_shift(image)
-
-        # image.save('trial.jpg')
         break
     
 
